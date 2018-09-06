e41edcc3fe603c610a0cc66815b367474ce1f112
mushi14 <smhassan@dons.usfca.edu>
2018-09-06 11:32:58 -0700

    Lab

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 /**
@@ -14,6 +19,13 @@ public class Driver {
 	 */
 	public static void main(String[] args) {
 		// TODO Fill in
+		Path path = Paths.get("/Users/mushahidhassan/Desktop/CS 212/ProjectTests/project-tests/text/simple/hello.txt");
+		try (BufferedReader br = Files.newBufferedReader(path)) {
+			System.out.println(br.readLine());
+		} catch (IOException e) {
+			System.out.println("file doesnt exist");
+		}
+		
 		System.out.println(Arrays.toString(args));
 	}
 
