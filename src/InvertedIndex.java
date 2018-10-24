ef4fd1c666b2e21d4aece26e94171d5c51e866d4
Mushahid Hassan <smhassan@dons.usfca.edu>
2018-10-24 00:25:45 -0700

    Final commit for Project 1. Ready for last release.

--- src/InvertedIndex.java
+++ src/InvertedIndex.java
@@ -38,25 +38,25 @@ public class InvertedIndex {
 	}
 
 	/**
-	 * TODO
-	 * @param word
-	 * @param path
-	 * @param position
+	 * Adds the word, the file it appears in, and the position it appears in to the inverted index
+	 * @param word word inside of the file
+	 * @param location path of the file
+	 * @param position position in which the word appears in the file
 	 */
-	public void add(String word, String path, int position) {
+	public void add(String word, String location, int position) {
 		if (index.containsKey(word)) {
-			if (index.get(word).containsKey(path)) {
-				if (!index.get(word).get(path).contains(position)) {
-					index.get(word).get(path).add(position);
+			if (index.get(word).containsKey(location)) {
+				if (!index.get(word).get(location).contains(position)) {
+					index.get(word).get(location).add(position);
 				}
 			} else {
-				index.get(word).put(path, new TreeSet<Integer>());
-				index.get(word).get(path).add(position);
+				index.get(word).put(location, new TreeSet<Integer>());
+				index.get(word).get(location).add(position);
 			}
 		} else {
 			index.put(word, new TreeMap<String, TreeSet<Integer>>());
-			index.get(word).put(path, new TreeSet<Integer>());
-			index.get(word).get(path).add(position);
+			index.get(word).put(location, new TreeSet<Integer>());
+			index.get(word).get(location).add(position);
 		}
 	}
 
@@ -100,13 +100,13 @@ public class InvertedIndex {
 	 * Shows all the positions associated with a path in the map
 	 *
 	 * @param word word inside of the file
-	 * @param path path of the file
+	 * @param location path of the file
 	 * @return Returns a set view of all the positions associated with the path
 	 */
-	public Set<Integer> getPositions(String word, String path) {
+	public Set<Integer> getPositions(String word, String location) {
 		if (containsWord(word)) {
-			if (containsPath(word, path)) {
-				return Collections.unmodifiableSet(index.get(word).get(path));
+			if (containsPath(word, location)) {
+				return Collections.unmodifiableSet(index.get(word).get(location));
 			} else {
 				return Collections.emptySet();
 			}
@@ -142,12 +142,12 @@ public class InvertedIndex {
 	 *  Number of positions associated with the path in the the map
 	 * 
 	 * @param word word inside of the file
-	 * @param path path of the file
+	 * @param location path of the file
 	 * @return integer size of the number of paths associated with word in the map
 	 */
-	public int positions(String word, String path) {
-		if (containsPath(word, path)) {
-			return index.get(word).get(path).size();
+	public int positions(String word, String location) {
+		if (containsPath(word, location)) {
+			return index.get(word).get(location).size();
 		} else {
 			return 0;
 		}
@@ -167,12 +167,12 @@ public class InvertedIndex {
 	 * Checks to see if the word contains the path
 	 * 
 	 * @param word word inside the file
-	 * @param path path of the file
+	 * @param location path of the file
 	 * @return true if word contains the path
 	 */
-	public boolean containsPath(String word, String path) {
+	public boolean containsPath(String word, String location) {
 		if (containsWord(word)) {
-			return index.get(word).containsKey(path);
+			return index.get(word).containsKey(location);
 		} else {
 			return false;
 		}
@@ -181,13 +181,13 @@ public class InvertedIndex {
 	/**
 	 * Checks to see if the specified word from the path contains the position
 	 * @param word word from the file
-	 * @param path file path the word is in
+	 * @param location file path the word is in
 	 * @param position specific position the word occurs in the file path
 	 * @return returns true if position exists in the path, false otherwise
 	 */
-	public boolean containsPosition(String word, String path, int position) {
-		if (containsPath(word, path)) {
-			return index.get(word).get(path).contains(position);
+	public boolean containsPosition(String word, String location, int position) {
+		if (containsPath(word, location)) {
+			return index.get(word).get(location).contains(position);
 		} else {
 			return false;
 		}
@@ -195,11 +195,11 @@ public class InvertedIndex {
 
 	/**
 	 * Writes the index to the file path in pretty json format
-	 * @param path path to the file to write to
+	 * @param location path to the file to write to
 	 * @throws IOException in case there's any problem finding the file
 	 */
-	public void writeIndexJSON(Path path) throws IOException {
-		TreeJSONWriter.asTripleNested(this.index, path);
+	public void writeIndexJSON(Path location) throws IOException {
+		TreeJSONWriter.asTripleNested(this.index, location);
 	}
 
 	/** 
