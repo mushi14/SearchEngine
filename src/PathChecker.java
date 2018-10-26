5894127fb84bf97001cfbcb956bd34883df24d40
Mushahid Hassan <smhassan@dons.usfca.edu>
2018-10-26 12:32:21 -0700

    updating master

--- src/PathChecker.java
+++ src/PathChecker.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PathChecker {
 
@@ -22,10 +24,33 @@ public class PathChecker {
 				}
 			}
 		} else if (Files.isRegularFile(path)) {
-			String name = path.toString().toLowerCase();
-			if (name.endsWith(".txt") || name.endsWith(".text")) {
+			String name = path.toString();
+			if (name.toLowerCase().endsWith(".txt") || name.toLowerCase().endsWith(".text")) {
 				TextFileStemmer.stemFile(path, index);
 			}
 		}
+	}
+
+	/**
+	 * Checks to see if the path provided for queries is a directory or file. 
+	 * @param path path to the file or directory
+	 * @return list of files valid query files
+	 * @throws IOException
+	 */
+	public static List<String> queryFiles(Path path) throws IOException {
+		List<String> files = new ArrayList<>();
+		if (Files.isDirectory(path)) {
+			try (DirectoryStream<Path> filePathStream = Files.newDirectoryStream(path)) {
+				for (Path file: filePathStream) {
+					queryFiles(file);
+				}
+			}
+		} else if (Files.isRegularFile(path)) {
+			String name = path.toString();
+			if (name.toLowerCase().endsWith(".txt") || name.toLowerCase().endsWith(".text")) {
+				files.add(name);
+			}
+		}
+		return files;
 	}
 }
