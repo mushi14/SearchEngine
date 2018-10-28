7a2db5b8a40646807be67fc2b94db74317f466c2
Mushahid Hassan <smhassan@dons.usfca.edu>
2018-10-28 13:36:45 -0700

    Trying to multithread building index from directory

--- src/MultithreadedPathChecker.java
+++ src/MultithreadedPathChecker.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,14 +13,21 @@ public class MultithreadedPathChecker {
 	Logger logger = LogManager.getLogger();
 
 	private final Set<String> paths;
+	ThreadSafeInvertedIndex index = new ThreadSafeInvertedIndex();
 
 	private MultithreadedPathChecker() {
 		paths = new HashSet<>();
 	}
 
-	private class FilesWorker implements Runnable {
+	private void parse(Path path) {
+		Thread worker = new FilesWorker(path);
+		worker.start();
+	}
+
+	private class FilesWorker extends Thread {
 
 		private Path path;
+
 		public FilesWorker(Path path) {
 			this.path = path;
 			logger.debug("Worker for {} created", path);
@@ -27,24 +35,25 @@ public class MultithreadedPathChecker {
 
 		@Override
 		public void run() {
-			if (Files.isDirectory(path)) {
-				try (DirectoryStream<Path> filePathStream = Files.newDirectoryStream(path)) {
-					for (Path file: filePathStream) {
-						paths.add(file.toString());
-						Thread worker = new FilesWorker(file);
-						worker.start();
-						logger.debug("New worker created and started for {}", file);
+			try {
+				if (Files.isDirectory(path)) {
+					try (DirectoryStream<Path> filePathStream = Files.newDirectoryStream(path)) {
+						for (Path file: filePathStream) {
+							paths.add(file.toString());
+							Thread worker = new FilesWorker(file);
+							worker.start();
+							logger.debug("New worker created and started for {}", file);
+						}
+					}
+				} else if (Files.isRegularFile(path)) {
+					String name = path.toString();
+					if (name.toLowerCase().endsWith(".txt") || name.toLowerCase().endsWith(".text")) {
+						paths.add(path.toString());
+//						TextFileStemmer.stemFile(path, index);
 					}
 				}
-			} else if (Files.isRegularFile(path)) {
-				String name = path.toString();
-				if (name.toLowerCase().endsWith(".txt") || name.toLowerCase().endsWith(".text")) {
-//					TextFileStemmer.stemFile(path, index);
-					paths.add(path.toString());
-					Thread worker = new FilesWorker(path);
-					worker.start();
-					logger.debug("New worker created and started for {}", path);
-				}
+			} catch (IOException e) {
+				logger.debug(e.getMessage(), e);
 			}
 
 			logger.debug("Worker for {} finished", path);
