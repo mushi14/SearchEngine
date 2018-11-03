30e36fc681742ce8a2f145b2689313b2ead21235
Mushahid Hassan <smhassan@dons.usfca.edu>
2018-11-03 07:45:51 -0700

    Updating project 3 branch

--- src/QueryFileParser.java
+++ src/QueryFileParser.java
@@ -8,7 +8,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 import java.util.TreeSet;
 
 import opennlp.tools.stemmer.Stemmer;
@@ -28,28 +27,43 @@ public class QueryFileParser {
 	 * @param path path of the file
 	 * @param exact boolean variable that ensures that an exact search must be performed
 	 */
-	public Map<String, List<Search>> stemQueryFile(InvertedIndex index, boolean exact) {
+	public Map<String, List<Search>> stemQueryFile(Path path, boolean exact) {
 		try (BufferedReader br = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
 			String line = br.readLine();
 			Stemmer stemmer = new SnowballStemmer(SnowballStemmer.ALGORITHM.ENGLISH);
-			Map<String, List<Search>> results = new TreeMap<>();
 
 			while (line != null) {
 				Set<String> queries = new TreeSet<>();
 				String[] words = TextFileStemmer.parse(line);
+
 				for (String word : words) {
 					word = stemmer.stem(word).toString();
 					queries.add(word);
 				}
+
 				if (!queries.isEmpty()) {
 					if (exact == true) {
-						index.exactSearch(results, queries);
+						String queryLine = String.join(" ", queries);
+						if (!results.containsKey(queryLine)) {
+							results.put(queryLine, new ArrayList<>());
+							for (Search query : index.exactSearch(queries)) {
+								results.get(queryLine).add(query);
+							}
+						}
 					} else {
-						index.partialSearch(results, queries);
+						String queryLine = String.join(" ", queries);
+						if (!results.containsKey(queryLine)) {
+							results.put(queryLine, new ArrayList<>());
+							for (Search query : index.partialSearch(queries)) {
+								results.get(queryLine).add(query);
+							}
+						}
 					}
 				}
+
 				line = br.readLine();
 			}
+
 			return results;
 
 		} catch (IOException | NullPointerException e) {
@@ -58,45 +72,54 @@ public class QueryFileParser {
 		}
 	}
 
+//	/**
+//	 * Stems query file performing partial or exact search and stores the results accordingly
+//	 * @param index inverted index that contains the words, their locations, and their positions
+//	 * @param path path of the file
+//	 * @param exact boolean variable that ensures that an exact search must be performed
+//	 */
+//	public Map<String, List<Search>> multithreadQueryFile(ThreadSafeInvertedIndex index, boolean exact, 
+//			int threads) {
+//		try (BufferedReader br = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+//			String line = br.readLine();
+//			Stemmer stemmer = new SnowballStemmer(SnowballStemmer.ALGORITHM.ENGLISH);
+//			List<Set<String>> temp = new ArrayList<>();
+//			Map<String, List<Search>> results = new TreeMap<>();
+//
+//			while (line != null) {
+//				Set<String> queries = new TreeSet<>();
+//				String[] words = TextFileStemmer.parse(line);
+//				for (String word : words) {
+//					word = stemmer.stem(word).toString();
+//					queries.add(word);
+//				}
+//				if (!queries.isEmpty()) {
+//					if (exact == true) {
+//						temp.add(queries);
+//					} else {
+////						index.partialSearch(results, queries);
+//					}
+//				}
+//				line = br.readLine();
+//			}
+//			
+//			MultithreadedExactSearch exactSearch = new MultithreadedExactSearch(index, results, threads, temp);
+////			System.out.println(exactSearch.results);
+//			
+//			return results;
+//
+//		} catch (IOException | NullPointerException e) {
+//			System.out.println("There was an issue finding the query file: " + path);
+//			return Collections.emptyMap();
+//		}
+//	}
+
 	/**
-	 * Stems query file performing partial or exact search and stores the results accordingly
-	 * @param index inverted index that contains the words, their locations, and their positions
-	 * @param path path of the file
-	 * @param exact boolean variable that ensures that an exact search must be performed
+	 * Writes the search results to the file path in pretty json format
+	 * @param path path to the file to write to
+	 * @throws IOException in case there's any problem finding the file
 	 */
-	public Map<String, List<Search>> multithreadQueryFile(ThreadSafeInvertedIndex index, boolean exact, 
-			int threads) {
-		try (BufferedReader br = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
-			String line = br.readLine();
-			Stemmer stemmer = new SnowballStemmer(SnowballStemmer.ALGORITHM.ENGLISH);
-			List<Set<String>> temp = new ArrayList<>();
-			Map<String, List<Search>> results = new TreeMap<>();
-
-			while (line != null) {
-				Set<String> queries = new TreeSet<>();
-				String[] words = TextFileStemmer.parse(line);
-				for (String word : words) {
-					word = stemmer.stem(word).toString();
-					queries.add(word);
-				}
-				if (!queries.isEmpty()) {
-					if (exact == true) {
-						temp.add(queries);
-					} else {
-//						index.partialSearch(results, queries);
-					}
-				}
-				line = br.readLine();
-			}
-			
-			MultithreadedExactSearch exactSearch = new MultithreadedExactSearch(index, results, threads, temp);
-//			System.out.println(exactSearch.results);
-			
-			return results;
-
-		} catch (IOException | NullPointerException e) {
-			System.out.println("There was an issue finding the query file: " + path);
-			return Collections.emptyMap();
-		}
+	public void writeJSON(Path path) throws IOException {
+		TreeJSONWriter.asSearchResult(results, path);
 	}
 }
