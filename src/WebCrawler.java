6bf8dee1a9fc892df0ac236464d44779992ed9cd
Mushahid Hassan <smhassan@dons.usfca.edu>
2018-11-18 11:54:10 -0800

    Trying to crawl the web

--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -1,22 +1,38 @@
+import java.io.IOException;
 import java.net.URL;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class WebCrawler {
+
+	private static final Logger logger = LogManager.getLogger();
 
 	private final ThreadSafeInvertedIndex threadSafeIndex;
 	private final WorkQueue queue;
 	private int pending;
+	private int count;
 
 	public WebCrawler(URL url, int total, int threads, ThreadSafeInvertedIndex threadSafeIndex) {
 		this.threadSafeIndex = threadSafeIndex;
 		this.queue = new WorkQueue(threads);
 		this.pending = 0;
+		this.count = 0;
 		this.start(url, total);
 		this.finish();
 		this.queue.shutdown();
 	}
 
 	private void start(URL url, int total) {
-		
+		count++;
+		if (count <= total) {
+			try {
+				String html = HTMLFetcher.fetchHTML(url);
+				LinkParser.listLinks(url, html);
+			} catch (IOException e) {
+				logger.debug("Exception with fetch html");
+			}
+		}
 	}
 
 	private synchronized void incrementPending() {
@@ -42,10 +58,15 @@ public class WebCrawler {
 	}
 
 	private class Crawler implements Runnable {
+		private String html;
+
+		private Crawler(String html) {
+			this.html = html;
+		}
 
 		@Override
 		public void run() {
-			
+			/* Parse the doctype html and store each word in the thread safe inverted index*/
 		}
 		
 	}
